85d92a20f67a189642b23c99be0b0d956947a305
OEM Configuration (temporary user) <hatunapatata@gmail.com>
2025-01-09 05:10:35 +0200

    fix bookmark list hook

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
-const url='https://yt-timestamp.onrender.com'
-// const url='http://localhost:10000'
+// const url = "https://yt-timestamp.onrender.com";
+const url = "http://localhost:10000";
 export const getCurrentActiveTab = async () => {
   let queryOptions = { active: true, lastFocusedWindow: true };
 
@@ -7,31 +7,17 @@ export const getCurrentActiveTab = async () => {
   return tab;
 };
 
-export const getVideoId = tab => {
-  const queryParameters = tab.url.split('?')[1];
+export const getVideoId = (tab) => {
+  const queryParameters = tab.url.split("?")[1];
   const urlParameters = new URLSearchParams(queryParameters);
-  return urlParameters.get('v');
+  return urlParameters.get("v");
 };
 
- export const getValueFromStorage = async (key, defaultResponse) => {
-   const result = await chrome.storage.sync.get([key]);
-    return result[key] ? JSON.parse(result[key]) : defaultResponse;
-  };
+export const getValueFromStorage = async (key, defaultResponse) => {
+  const result = await chrome.storage.local.get([key]);
+  return result[key] ? JSON.parse(result[key]) : defaultResponse;
+};
 
- export const setValueToStorage = async (value, key) => {
-    await chrome.storage.sync.set({[key]: JSON.stringify(value)});
-    console.log('key',key)
-    const data=await chrome.storage.sync.get()
-    // console.log('predata2',data[key])
-    // const bookmarks=JSON.parse(data)
-    console.log('got data2',data)
-    //http://localhost:10000
-    const res=await fetch(`${url}/app/hook`,{
-      method:'post',
-      headers:{'content-type':'application/json'},
-      body:JSON.stringify(data)
-  })
-      const resData=await res.json()
-      console.log('resData2',resData)
-  
-  };
+export const setValueToStorage = async (value, key) => {
+  await chrome.storage.local.set({ [key]: JSON.stringify(value) });
+};
--- youtubeContentScript.js
+++ youtubeContentScript.js
@@ -1,104 +1,108 @@
 (() => {
   let youtubeRightControls, youtubePlayer, isTitlePause;
-  let currentVideo = '',
+  let currentVideo = "",
     currentVideoBookmarks = [];
   const currentVideoData = {};
-  let bookmarkTitle = '';
-  // const url='http://localhost:10000'
-  const url='https://yt-timestamp.onrender.com'
-  console.log('url',url)
-  let timerId=0
+  let bookmarkTitle = "";
+  const url = "http://localhost:10000";
+  // const url = "https://yt-timestamp.onrender.com";
+  console.log("url", url);
+  let timerId = 0;
   // listner for all emit messages
   chrome.runtime.onMessage.addListener(async (obj, sender, response) => {
     const { type, value, videoId, settings } = obj;
 
-    if (type === 'NEW') {
+    if (type === "NEW") {
       currentVideo = videoId;
       isTitlePause = settings.isTitlePause;
       currentVideoData.id = videoId;
-      addSnippetToVideoData(currentVideoData)
+      // addSnippetToVideoData(currentVideoData);
 
       newVideoLoaded();
-    } else if (type === 'PLAY') {
+    } else if (type === "PLAY") {
       youtubePlayer.currentTime = value;
-    } else if (type === 'DELETE') {
+    } else if (type === "DELETE") {
       currentVideoBookmarks = currentVideoBookmarks.filter(
-        bookmark => bookmark.time != value
+        (bookmark) => bookmark.time != value
       );
       setValueToStorage(
         { ...currentVideoData, bookmarks: currentVideoBookmarks },
         currentVideo
       );
-
-      response(currentVideoBookmarks);
+      addOrDeleteBookmarkOnServer({
+        videoId: currentVideo,
+        time: value,
+        operationType: type,
+      });
     }
+    response(currentVideoBookmarks);
   });
 
   // reusable methods
   const getValueFromStorage = async (key, defaultResponse) => {
-    const result = await chrome.storage.sync.get([key]);
+    const result = await chrome.storage.local.get([key]);
     return result[key] ? JSON.parse(result[key]) : defaultResponse;
   };
 
-  const setValueToStorage = async(value, key) => {
-    await chrome.storage.sync.set({ [key]: JSON.stringify(value) });
-    const data=await chrome.storage.sync.get()
-    const res=await fetch(`${url}/app/hook`,{
-      method:'post',
-      headers:{'content-type':'application/json'},
-      body:JSON.stringify(data)
-  })
-      await res.json()  
+  const setValueToStorage = async (value, key) => {
+    await chrome.storage.local.set({ [key]: JSON.stringify(value) });
+    // const data = await chrome.storage.local.get();
+    // const res = await fetch(`${url}/app/hook`, {
+    //   method: "post",
+    //   headers: { "content-type": "application/json" },
+    //   body: JSON.stringify(data),
+    // });
+    // await res.json();
   };
 
   // Methods
   const fetchCurrentVideoData = async () => {
     try {
       const data = await getValueFromStorage(currentVideo, {});
-      currentVideoData['bookmarks'] = data.bookmarks;
-      currentVideoData['isTitlePause'] = data.isTitlePause;
+      currentVideoData["bookmarks"] = data.bookmarks;
+      currentVideoData["isTitlePause"] = data.isTitlePause;
 
       return data;
     } catch (error) {
-      console.log('context validate',error.message);
+      console.log("context validate", error.message);
       sendReloadSignal();
     }
   };
 
   const newVideoLoaded = async () => {
     const bookmarkBtnExists =
-      document.getElementsByClassName('bookmark-btn')[0];
+      document.getElementsByClassName("bookmark-btn")[0];
     const data = await fetchCurrentVideoData();
     currentVideoBookmarks = data.bookmarks ? data.bookmarks : [];
     if (!bookmarkBtnExists) {
-      const bookmarkBtn = document.createElement('img');
-      const iconWrapper = document.createElement('span');
-      const bookmarkContainer = document.createElement('span');
+      const bookmarkBtn = document.createElement("img");
+      const iconWrapper = document.createElement("span");
+      const bookmarkContainer = document.createElement("span");
 
-      bookmarkContainer.className = 'ytp-button';
-      iconWrapper.className = 'icon-wrapper';
+      bookmarkContainer.className = "ytp-button";
+      iconWrapper.className = "icon-wrapper";
 
-      bookmarkBtn.src = chrome.runtime.getURL('assets/bookmark.png');
-      bookmarkBtn.className = 'ytp-button ' + 'bookmark-btn';
-      bookmarkBtn.title = 'Click to bookmark';
+      bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+      bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+      bookmarkBtn.title = "Click to bookmark";
 
       youtubeRightControls =
-        document.getElementsByClassName('ytp-right-controls')[0];
-      youtubePlayer = document.getElementsByClassName('video-stream')[0];
+        document.getElementsByClassName("ytp-right-controls")[0];
+      youtubePlayer = document.getElementsByClassName("video-stream")[0];
 
       iconWrapper.appendChild(bookmarkBtn);
       bookmarkContainer.appendChild(iconWrapper);
-      console.log('right controls',youtubeRightControls)
+      console.log("right controls", youtubeRightControls);
       youtubeRightControls.insertBefore(
         bookmarkContainer,
         youtubeRightControls.firstChild
       );
       bookmarkContainer;
-      bookmarkBtn.addEventListener('click', addNewBookmarkEventHandler);
+      bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
     }
   };
 
-  const getTime = t => {
+  const getTime = (t) => {
     const date = new Date(0);
     date.setSeconds(t);
 
@@ -114,23 +118,23 @@
 
       isTitlePause ? addBookmark() : openModal();
     } catch (error) {
-      console.log('addbookmark error',error.message);
+      console.log("addbookmark error", error.message);
       sendReloadSignal();
     }
   };
 
   const addBookmark = async () => {
     if (isTitlePause) {
-      const bookmarkBtn = document.getElementsByClassName('bookmark-btn')[0];
-      const saveBtnSrc = chrome.runtime.getURL('assets/bookmark.gif');
-      const addbookmarkBtnSrc = chrome.runtime.getURL('assets/bookmark.png');
+      const bookmarkBtn = document.getElementsByClassName("bookmark-btn")[0];
+      const saveBtnSrc = chrome.runtime.getURL("assets/bookmark.gif");
+      const addbookmarkBtnSrc = chrome.runtime.getURL("assets/bookmark.png");
 
       bookmarkBtn.src = saveBtnSrc;
       setTimeout(() => {
         bookmarkBtn.src = addbookmarkBtnSrc;
       }, 1000);
     }
-    
+
     // const title=document.getElementById('above-the-fold').querySelector('yt-formatted-string').textContent
     // // const channelName=document.getElementById('text').querySelector('a').getAttribute('href')
     // const channelName=document.getElementById('channel-name').querySelector('a').getAttribute('href')
@@ -143,10 +147,9 @@
     const newBookmark = {
       time: currentTime,
       desc:
-        bookmarkTitle != ''
+        bookmarkTitle != ""
           ? bookmarkTitle
-          : `Bookmark at ${getTime(currentTime)}`
-      
+          : `Bookmark at ${getTime(currentTime)}`,
     };
     // fetch latest bookmarks
     const data = await fetchCurrentVideoData();
@@ -164,15 +167,20 @@
     currentVideoData.bookmarks = updatedVideoData.bookmarks;
     currentVideoData.isTitlePause = updatedVideoData.isTitlePause;
     setValueToStorage(currentVideoData, currentVideo);
-
-    bookmarkTitle = '';
+    addOrDeleteBookmarkOnServer({
+      videoId: currentVideo,
+      time: newBookmark.time,
+      operationType: "NEW",
+      timestampTitle: newBookmark.desc,
+    });
+    bookmarkTitle = "";
   };
 
   const openModal = () => {
     youtubePlayer.pause();
 
-    const modal = document.createElement('dialog');
-    modal.id = 'dialog';
+    const modal = document.createElement("dialog");
+    modal.id = "dialog";
     modal.innerHTML = `
   <div class="input">
     <input type="text" class="name" id="bookmarkTitle" placeholder="Enter bookmark title" />
@@ -180,31 +188,31 @@
   </div>`;
 
     document.body.appendChild(modal);
-    const dialog = document.querySelector('dialog');
+    const dialog = document.querySelector("dialog");
     dialog.showModal();
 
     // Reset the input value
-    const inputField = document.getElementById('bookmarkTitle');
-    inputField.value = '';
+    const inputField = document.getElementById("bookmarkTitle");
+    inputField.value = "";
     inputField.focus();
-    bookmarkTitle = '';
+    bookmarkTitle = "";
 
     // Close the modal when clicking anywhere on the screen
-    document.getElementById('dialog').addEventListener('click', event => {
+    document.getElementById("dialog").addEventListener("click", (event) => {
       if (event.target === dialog) closeModal();
     });
 
     // Close the modal when clicking anywhere on the screen
-    document.getElementById('add-btn').addEventListener('click', () => {
+    document.getElementById("add-btn").addEventListener("click", () => {
       closeModal();
       addBookmark();
     });
     // close modal when pressing enter key
-    document.addEventListener('keydown', handleKeyPress);
+    document.addEventListener("keydown", handleKeyPress);
   };
 
   const closeModal = () => {
-    const modal = document.getElementById('dialog');
+    const modal = document.getElementById("dialog");
     if (!modal) return;
 
     // get bookmark title from field
@@ -213,34 +221,32 @@
     modal.parentNode.removeChild(modal);
 
     // removing the listner from document
-    document.removeEventListener('keydown', handleKeyPress);
+    document.removeEventListener("keydown", handleKeyPress);
 
     // resume the video
     youtubePlayer.play();
   };
 
-  const handleKeyPress = event => {
-    if (event.key === 'Enter') {
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
       closeModal();
       addBookmark();
     }
   };
 
   const getBookMarkTitle = () => {
-    const inputField = document.getElementById('bookmarkTitle');
+    const inputField = document.getElementById("bookmarkTitle");
     bookmarkTitle = inputField?.value;
   };
 
   const getCurrentVideoDetail = () => {
-
     const title = document.querySelector(
-      '.title > .ytd-video-primary-info-renderer'
+      ".title > .ytd-video-primary-info-renderer"
     ).innerText;
     const thumbnail = `http://img.youtube.com/vi/${currentVideo}/0.jpg`;
 
-    currentVideoData['title'] = title;
-    currentVideoData['thumbnail'] = thumbnail;
-    
+    currentVideoData["title"] = title;
+    currentVideoData["thumbnail"] = thumbnail;
   };
 
   // runs if there is any context invalidate error occurs
@@ -248,36 +254,30 @@
     window.location.reload();
   };
 
-  async function addSnippetToVideoData (currentVideoData){
+  async function addSnippetToVideoData(currentVideoData) {
+    try {
+      const res = await fetch(`${url}/app/video/${currentVideo}`, {
+        headers: { "content-type": "application/json" },
+      });
+      const snippet = await res.json();
+      Object.assign(currentVideoData, snippet);
+      // const ,channelId,channelTitle:channelName,description}=snippet
+    } catch (err) {
+      console.log("error fetching endpoint", err);
+    }
+  }
 
-    try{
-    const res=await fetch(`${url}/app/video/${currentVideo}`,{
-      headers:{'content-type':'application/json'},
-    })
-    const snippet=await res.json()
-    Object.assign(currentVideoData,snippet)
-    // const ,channelId,channelTitle:channelName,description}=snippet
-}catch(err){
-  console.log('error fetching endpoint',err)
-}
-}
-}
-
-)();
-
-
-window.addEventListener('load',()=>{
-  console.log('window loaded')
-  console.log('title',document.querySelector('#above-the-fold yt-formatted-string'))
-  // timerId=setInterval(()=>{
-  //   console.log('inside timer',areAllDetailsLoaded)
-  //   if(!areAllDetailsLoaded)return;
-  //   clearInterval(timerId)
-  // currentVideoData.title=document.querySelector('#above-the-fold yt-formatted-string').textContent
-  //     currentVideoData.channelName=document.querySelector('#text a').getAttribute('href')
-  //     currentVideoData.location=location
-  //     currentVideoData.description=document.querySelector('yt-attributed-string-span').textContent
-  //   },200)
-  
-})
-
+  async function addOrDeleteBookmarkOnServer(data) {
+    try {
+      const res = await fetch(`${url}/app/hook`, {
+        method: "post",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(data),
+      });
+      await res.json();
+      console.log("sent data");
+    } catch (err) {
+      console.log("error in addOrDeleteBookmarkOnServer", err);
+    }
+  }
+})();
